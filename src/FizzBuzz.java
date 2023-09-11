c005b9d7b32d5c5f2a4e47c247c5a4d224ccfc0c
Daniel Niu <danielfqn@gmail.com>
2023-09-11 18:42:25 -0400

    refactored to call FizzBuzz.main() from Main.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,16 +3,20 @@
  */
 class FizzBuzz {
 
+    static void mainCaller(){
+        main(null);
+    }
+
     public static void main(String[] args) {
         int i = 1;
         while (i < 100) {
 //        for (int i = 1; i < 100; i++) {
-
             i = doFizzBuzz(i);
+            i++;
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
@@ -35,7 +39,6 @@ class FizzBuzz {
             System.out.println(i);
 
         }
-        i++;
         return i;
     }
 }
